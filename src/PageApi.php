9c03c3d768c1354c28c421ee5d84fab179e1b1f6
Justin Shroeder <man4mac@gmail.com>
2017-03-03 15:51:31 -0500

    Fixes formatting error with X-Render-Host header

--- src/PageApi.php
+++ src/PageApi.php
@@ -33,7 +33,7 @@ class PageApi
         $this->http = new Client([
             'base_uri' => $config['base_uri'],
             'headers' => [
-                'X-Render-Host: ' . $request->getUri()->getHost(),
+                'X-Render-Host' => $request->getUri()->getHost(),
             ]
         ]);
     }
