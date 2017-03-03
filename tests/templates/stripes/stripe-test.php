5ed2f68847b178218e4c13c38138766007ddba13
Joshua Bryant <josh.m.b88@gmail.com>
2017-03-03 15:14:18 -0500

    Adds `inline($str)` method to Renderer.php (with test)

--- tests/templates/stripes/stripe-test.php
+++ tests/templates/stripes/stripe-test.php
@@ -1,4 +1,5 @@
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     valid: 3xnf}yxFwVHCsXR8p3BRBRZQi2
     <?= $this->e($this->image('123.jpg', 'sepia')) ?>
+    <?= $this->inline('<a><p>Test</p></a>') ?>
 </div>
