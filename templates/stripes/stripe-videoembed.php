42efd631f48af27dbcfb531f18620c630cd25af5
Emily Merricks <em2cp@virginia.edu>
2017-08-21 10:01:31 -0400

    Correctly accesses video embed links

--- templates/stripes/stripe-videoembed.php
+++ templates/stripes/stripe-videoembed.php
@@ -1,7 +1,7 @@
-<?php if (!empty($vembed)) : ?>
+<?php if (!empty($embed)) : ?>
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
-        <div class="vssl-stripe--videoembed--video"><?= $vembed ?></div>
+        <div class="vssl-stripe--videoembed--video"><?= $embed ?></div>
     </div>
 </div>
 <?php endif; ?>
