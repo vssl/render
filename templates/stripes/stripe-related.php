b22a09c3a34147602e0fa4ca23b8ba1a7973175d
Emily Merricks <em2cp@virginia.edu>
2017-10-18 16:21:13 -0400

    Renames References stripe to Related

---
+++ templates/stripes/stripe-related.php
@@ -0,0 +1,33 @@
+<?php
+$links = array_values(array_filter($links, function ($link) {
+        return !empty($link['page_id']);
+}));
+?>
+<?php if (count($links)) : ?>
+<div class="<?= $this->e($type, 'wrapperClasses') ?>">
+    <div class="vssl-stripe-column">
+        <?php if (!empty($label['html'])) : ?>
+        <h2 class="vssl-stripe--related--label"><?= $label['html'] ?></h2>
+        <?php endif;?>
+        <div class="vssl-stripe--related--links">
+            <?php foreach ($links as $idx => $link) : ?>
+                <div class="vssl-stripe--related--link">
+                    <a href="<?= $link['page']['slug'] ?>" target="_blank">
+                        <?php if (!empty($link['page']['image'])) : ?>
+                        <img class="vssl-stripe--related--thumbnail" src="<?= $this->image($link['page']['image']) ?>" alt="<?= $link['page']['image'] ?>">
+                        <?php endif; ?>
+
+                        <div class="vssl-stripe--related--text">
+                            <div class="vssl-stripe--related--page-info">
+                                <h3 class="vssl-stripe--related--title"><?= $link['page']['title'] ?></h3>
+                                <p class="vssl-stripe--related--description"><?= $link['page']['summary'] ?></p>
+                            </div>
+                            <p class="vssl-stripe--related--url"><?= $_SERVER['SERVER_NAME'] . $link['page']['slug'] ?></p>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
