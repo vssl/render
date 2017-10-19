18e9b330c8be03c6a6a1e3077021d43b605972d6
Joshua Bryant <josh.m.b88@gmail.com>
2017-10-19 15:31:42 -0400

    Changes Lede Stripe to only render if has content

--- templates/stripes/stripe-lede.php
+++ templates/stripes/stripe-lede.php
@@ -1,7 +1,7 @@
+<?php if (!empty($text['html'])) : ?>
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
-        <?php if (!empty($text['html'])) : ?>
         <div class="vssl-stripe--lede--text"><?= $text['html'] ?></div>
-        <?php endif; ?>
     </div>
 </div>
+<?php endif; ?>
