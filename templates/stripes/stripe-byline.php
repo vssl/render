16f00bf81b7c0ae01de04e1c44ea3e45a8e302cf
Joshua Bryant <josh.m.b88@gmail.com>
2017-10-19 15:26:00 -0400

    Updates Byline Stripe to use <div>s instead of <p>s

--- templates/stripes/stripe-byline.php
+++ templates/stripes/stripe-byline.php
@@ -4,15 +4,15 @@
         <div class="vssl-stripe--byline-fieldwrap">
             <?php if (!empty($author['html'])) : ?>
             <div class="vssl-stripe--byline-field vssl-stripe--byline--author">
-                <p class="vssl-stripe--byline-label">Written by:</p>
-                <p class="vssl-stripe--byline-value"><?= $this->inline($author['html']) ?></p>
+                <div class="vssl-stripe--byline-label">Written by:</div>
+                <div class="vssl-stripe--byline-value"><?= $this->inline($author['html']) ?></div>
             </div>
             <?php endif; ?>
 
             <?php if (!empty($date['html'])) : ?>
             <div class="vssl-stripe--byline-field vssl-stripe--byline--date">
-                <p class="vssl-stripe--byline-label">Published:</p>
-                <p class="vssl-stripe--byline-value"><?= $this->inline($date['html']) ?></p>
+                <div class="vssl-stripe--byline-label">Published:</div>
+                <div class="vssl-stripe--byline-value"><?= $this->inline($date['html']) ?></div>
             </div>
             <?php endif; ?>
         </div>
