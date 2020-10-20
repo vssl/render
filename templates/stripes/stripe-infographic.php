c377ed04ddf0e2f280e8833cbc31092c3c81e73e
Joshua Bryant <joshb@journeygroup.com>
2020-10-20 12:04:05 -0400

    stripe(infographic): add enlarge button

--- templates/stripes/stripe-infographic.php
+++ templates/stripes/stripe-infographic.php
@@ -1,5 +1,8 @@
 <?php if (!empty($image)) : ?>
-<div class="<?= $this->e($type, 'wrapperClasses') ?>">
+<div
+    class="<?= $this->e($type, 'wrapperClasses') ?>"
+    <?= !isset($is_enlargeable) || $is_enlargeable ? 'data-is-enlargeable="true"' : '' ?>
+>
     <div class="vssl-stripe-column">
         <?php if (!empty($title['html'])) : ?>
         <div class="vssl-stripe--infographic--title"><?= $this->inline($title['html']) ?></div>
@@ -17,6 +20,12 @@
             <?php if (!empty($credit['html'])) : ?>
             <div class="vssl-stripe--infographic--credit"><?= $this->inline($credit['html']) ?></div>
             <?php endif; ?>
+        </div>
+        <?php endif; ?>
+
+        <?php if (!isset($is_enlargeable) || $is_enlargeable): ?>
+        <div class="vssl-stripe--infographic--actions">
+            <button type="button" class="vssl-stripe--infographic--enlarge">Enlarge</button>
         </div>
         <?php endif; ?>
     </div>
