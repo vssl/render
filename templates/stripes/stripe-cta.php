ca0adc80e6093542651a380707e363a7722ee9c3
Joshua Bryant <joshb@journeygroup.com>
2020-10-20 16:03:03 -0400

    stripe: add card wrappers to contact, cta, and file

--- templates/stripes/stripe-cta.php
+++ templates/stripes/stripe-cta.php
@@ -1,21 +1,23 @@
 <?php if (!empty($first['html']) || !empty($second['html']) || (!empty($btn) && !empty($btntxt) && !empty($btnurl))): ?>
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
-        <div class="vssl-stripe--cta--text">
-            <?php if (!empty($first['html'])) : ?>
-            <p class="vssl-stripe--cta--first"><?= $this->inline($first['html']) ?></p>
-            <?php endif; ?>
+        <div class="vssl-stripe--cta--card vssl-stripe--card">
+            <div class="vssl-stripe--cta--text">
+                <?php if (!empty($first['html'])) : ?>
+                <p class="vssl-stripe--cta--first"><?= $this->inline($first['html']) ?></p>
+                <?php endif; ?>
 
-            <?php if (!empty($second['html'])) : ?>
-            <p class="vssl-stripe--cta--second"><?= $this->inline($second['html']) ?></p>
-            <?php endif; ?>
-        </div>
+                <?php if (!empty($second['html'])) : ?>
+                <p class="vssl-stripe--cta--second"><?= $this->inline($second['html']) ?></p>
+                <?php endif; ?>
+            </div>
 
-        <?php if (!empty($btn) && !empty($btntxt) && !empty($btnurl)): ?>
+            <?php if (!empty($btn) && !empty($btntxt) && !empty($btnurl)): ?>
             <div class="vssl-stripe--cta--button">
                 <a href="<?= $btnurl ?>" class="vssl-button"><?= $btntxt ?></a>
             </div>
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 <?php endif; ?>
