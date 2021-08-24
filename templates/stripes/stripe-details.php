269455a9369998a398577b617f54caaf871e8fd8
Joshua Bryant <joshb@journeygroup.com>
2021-08-24 09:58:47 -0400

    stripe(details): add `subtitle` to details summary

--- templates/stripes/stripe-details.php
+++ templates/stripes/stripe-details.php
@@ -2,7 +2,13 @@
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
         <details<?= !empty($open) && $open ? ' open' : '' ?>>
-            <summary class="vssl-stripe--details--summary"><?= $summary['html'] ?></summary>
+            <summary class="vssl-stripe--details--summary">
+                <h1 class="vssl-stripe--details--hed"><?= $summary['html'] ?></h1>
+
+                <?php if (!empty($subtitle['html'])) : ?>
+                <div class="vssl-stripe--details--subtitle"><?= $subtitle['html'] ?></div>
+                <?php endif; ?>
+            </summary>
             <div class="vssl-stripe--details--content"><?= $content['html'] ?></div>
         </details>
     </div>
