6057848b60094ead571e32d0d0e1457c2ac5b63d
Parker McCrary <parker.mccrary@journeygroup.com>
2022-04-01 10:26:08 -0400

    fix(stripes): add defensive code and update class names

--- templates/stripes/stripe-attributes.php
+++ templates/stripes/stripe-attributes.php
@@ -1,14 +1,18 @@
-<div class="<?= $this->e($type, 'wrapperClasses') ?>">
-  <div class="vssl-stripe-column">
-    <div class="vssl-stripe--attributes-fieldwrap">
-      <?php foreach ($values as $key => $value) : ?>
-        <div class="vssl-stripe--attributes-field vssl-stripe--attributes--<?= $key ?>">
-          <div class="vssl-stripe--attributes-label"><?= $key ?>:</div>
-          <div class="vssl-stripe--attributes-value">
-            <?= $value['html'] ?>
+<?php if (!empty($values)) : ?>
+  <div class="<?= $this->e($type, 'wrapperClasses') ?>">
+    <div class="vssl-stripe-column">
+      <div class="vssl-stripe--attributes--fieldwrap">
+        <?php foreach ($values as $key => $value) : ?>
+          <?php if (!empty($value['html']) ? $value['html'] : '') : ?>
+          <div class="vssl-stripe--attributes--field vssl-stripe--attributes--field--<?= $key ?>">
+            <div class="vssl-stripe--attributes--label"><?= $key ?>:</div>
+            <div class="vssl-stripe--attributes--value">
+              <?= $value['html'] ?>
+            </div>
           </div>
-        </div>
-      <?php endforeach; ?>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
-</div>
+<?php endif; ?>
