21b1cab651a6e11bf0d5c9b4b581b86aac664749
Parker McCrary <parker.mccrary@journeygroup.com>
2022-04-01 11:56:12 -0400

    fix(stripes): update values check to see if all values are null

--- templates/stripes/stripe-attributes.php
+++ templates/stripes/stripe-attributes.php
@@ -1,15 +1,21 @@
-<?php if (!empty($values)) : ?>
+<?php
+$isEmpty = array_reduce($values, function ($arePreviousEmpty, $value) {
+  return $arePreviousEmpty && is_null($value);
+}, true);
+?>
+
+<?php if (!$isEmpty) : ?>
   <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
       <div class="vssl-stripe--attributes--fieldwrap">
         <?php foreach ($values as $key => $value) : ?>
-          <?php if (!empty($value['html']) ? $value['html'] : '') : ?>
-          <div class="vssl-stripe--attributes--field vssl-stripe--attributes--field--<?= $key ?>">
-            <div class="vssl-stripe--attributes--label"><?= $key ?>:</div>
-            <div class="vssl-stripe--attributes--value">
-              <?= $value['html'] ?>
+          <?php if (!empty($value['html'])) : ?>
+            <div class="vssl-stripe--attributes--field vssl-stripe--attributes--field--<?= $key ?>">
+              <div class="vssl-stripe--attributes--label"><?= $key ?>:</div>
+              <div class="vssl-stripe--attributes--value">
+                <?= $this->inline($value['html'], '<a><b><strong><i><em><address>') ?>
+              </div>
             </div>
-          </div>
           <?php endif; ?>
         <?php endforeach; ?>
       </div>
