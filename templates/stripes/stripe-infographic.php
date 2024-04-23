1fa0b6ef2da46ae93810a1da95b4451b09c5a9f6
Brian McMillen <brian.mcmillen@journeygroup.com>
2024-04-23 09:45:39 -0400

    fix: include js in template for infographic stripe to display the image fullscreen

--- templates/stripes/stripe-infographic.php
+++ templates/stripes/stripe-infographic.php
@@ -20,7 +20,7 @@
             <?php if (!empty($caption['html'])) : ?>
             <div class="vssl-stripe--infographic--caption"><?= $this->inline($caption['html']) ?></div>
             <?php endif; ?>
-            
+
             <?php if (!empty($credit['html'])) : ?>
             <div class="vssl-stripe--infographic--credit"><?= $this->inline($credit['html']) ?></div>
             <?php endif; ?>
@@ -33,5 +33,39 @@
         </div>
         <?php endif; ?>
     </div>
+    <div class="vssl-stripe--infographic--lightbox">
+        <div class="vssl-stripe--infographic--image">
+            <img
+                src="<?= $this->image($image, !empty($image_style) ? $image_style : null) ?>"
+                alt="<?= $image ?>"
+                loading="lazy"
+            />
+        </div>
+        <button type="button" class="vssl-stripe--infographic--collapse">Close</button>
+    </div>
 </div>
+<script>
+const infographicEl = document.currentScript.previousElementSibling
+const enlargener = infographicEl.querySelector('.vssl-stripe--infographic--enlarge')
+const collapser = infographicEl.querySelector('.vssl-stripe--infographic--collapse')
+
+function onEnlarge() {
+    infographicEl.setAttribute('data-is-enlarged', true)
+    document.documentElement.classList.add('vssl-scroll-lock')
+    document.body.addEventListener('keyup', onInfographicKeyup)
+}
+
+function onCollapse() {
+    infographicEl.removeAttribute('data-is-enlarged')
+    document.documentElement.classList.remove('vssl-scroll-lock')
+    document.body.removeEventListener('keyup', onInfographicKeyup)
+}
+
+function onInfographicKeyup(e) {
+    if (e.key === 'Escape') onCollapse()
+}
+
+enlargener.addEventListener('click', onEnlarge)
+collapser.addEventListener('click', onCollapse)
+</script>
 <?php endif; ?>
