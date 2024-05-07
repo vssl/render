e5e97d3ea33f805009a26d64c51763529a54b427
Brian McMillen <brian.mcmillen@journeygroup.com>
2024-05-07 15:29:53 -0400

    fix: add the vssl-stripe--table wrapper class and fix caption output

--- templates/stripes/stripe-table.php
+++ templates/stripes/stripe-table.php
@@ -1,39 +1,43 @@
-<div class="vssl-stripe-column">
-  <table>
-    <?php if (!empty($caption)): ?>
-      <caption><?= htmlspecialchars($caption) ?></caption>
-    <?php endif; ?>
+<div class="<?= $this->e($type, 'wrapperClasses') ?>">
+  <div class="vssl-stripe-column">
+    <table>
+      <?php if (!empty($caption['html'])): ?>
+        <caption>
+          <?= $this->inline($caption['html']) ?>
+        </caption>
+      <?php endif; ?>
 
-    <?php if ($headersInFirstRow && !$headersInFirstColumn): ?>
-      <thead>
-        <?php foreach ($dataset[0] as $index => $item): ?>
-          <th><?= $item ?></th>
-        <?php endforeach; ?>
-      </thead>
-    <?php endif; ?>
-
-    <tbody>
-      <?php if ($headersInFirstRow && $headersInFirstColumn): ?>
-        <tr>
+      <?php if ($headersInFirstRow && !$headersInFirstColumn): ?>
+        <thead>
           <?php foreach ($dataset[0] as $index => $item): ?>
             <th><?= $item ?></th>
           <?php endforeach; ?>
-        </tr>
+        </thead>
       <?php endif; ?>
 
-      <?php
-        $rows = $headersInFirstRow ? array_slice($dataset, 1) : $dataset;
-        foreach ($rows as $rowIndex => $row):
-      ?>
-        <tr>
-          <?php foreach ($row as $index => $item): ?>
-            <?php $tag = ($index === 0 && $headersInFirstColumn) ? 'th' : 'td'; ?>
-            <<?= $tag ?>>
-              <?= $item ?>
-            </<?= $tag ?>>
-          <?php endforeach; ?>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
+      <tbody>
+        <?php if ($headersInFirstRow && $headersInFirstColumn): ?>
+          <tr>
+            <?php foreach ($dataset[0] as $index => $item): ?>
+              <th><?= $item ?></th>
+            <?php endforeach; ?>
+          </tr>
+        <?php endif; ?>
+
+        <?php
+          $rows = $headersInFirstRow ? array_slice($dataset, 1) : $dataset;
+          foreach ($rows as $rowIndex => $row):
+        ?>
+          <tr>
+            <?php foreach ($row as $index => $item): ?>
+              <?php $tag = ($index === 0 && $headersInFirstColumn) ? 'th' : 'td'; ?>
+              <<?= $tag ?>>
+                <?= $item ?>
+              </<?= $tag ?>>
+            <?php endforeach; ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
 </div>
