e404c3cdd190469aa365793004e8a1f68f7dc31f
Brian McMillen <brian.mcmillen@journeygroup.com>
2024-06-14 11:09:33 -0400

    fix: update the reference default stripe class names

--- templates/stripes/stripe-reference--default.php
+++ templates/stripes/stripe-reference--default.php
@@ -1,26 +1,26 @@
 
 <div class="vssl-stripe-column">
-  <div class="vssl-stripe--related--link">
+  <div class="vssl-stripe--reference--link">
     <a href="<?= $referencePage['slug'] ?>" target="_blank">
       <?php if (!empty($referencePage['image'])) : ?>
         <img
-          class="vssl-stripe--related--thumbnail"
+          class="vssl-stripe--reference--thumbnail"
           src="<?= $this->image($referencePage['image'], !empty($image_style) ? $image_style : null) ?>"
           alt="<?= $referencePage['image'] ?>"
           loading="lazy"
         />
       <?php endif; ?>
 
-      <div class="vssl-stripe--related--text">
-        <div class="vssl-stripe--related--page-info">
-          <h3 class="vssl-stripe--related--title">
+      <div class="vssl-stripe--reference--text">
+        <div class="vssl-stripe--reference--page-info">
+          <h3 class="vssl-stripe--reference--title">
             <?= $referencePage['title'] ?>
           </h3>
-          <p class="vssl-stripe--related--description">
+          <p class="vssl-stripe--reference--description">
             <?= $referencePage['summary'] ?>
           </p>
         </div>
-        <p class="vssl-stripe--related--url">
+        <p class="vssl-stripe--reference--url">
           <?= $_SERVER['SERVER_NAME'] . $referencePage['slug'] ?>
         </p>
       </div>
