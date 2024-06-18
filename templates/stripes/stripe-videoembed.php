b53e9669ac0fcc70da0f4da354d26c81220a86bb
Joshua Bryant <joshb@journeygroup.com>
2024-06-18 14:56:45 -0400

    feat(videoembed): add transcript, caption, and credit fields

--- templates/stripes/stripe-videoembed.php
+++ templates/stripes/stripe-videoembed.php
@@ -2,6 +2,30 @@
 <div class="<?= $this->e($type, 'wrapperClasses') ?>">
     <div class="vssl-stripe-column">
         <div class="vssl-stripe--videoembed--video"><?= $embed ?></div>
+        
+        <?php if (!empty($transcript['html'])) : ?>
+        <div class="vssl-stripe--videoembed--transcript">
+            <details class="vssl-stripe--videoembed--transcript--details">
+                <summary class="vssl-stripe--videoembed--transcript--summary">
+                    Read transcript
+                </summary>
+                <div class="vssl-stripe--videoembed--transcript--contents">
+                    <?= $transcript['html'] ?>
+                </div>
+            </details>
+        </div>
+        <?php endif ?>
+
+        <?php if (!empty($caption['html']) || !empty($credit['html'])) : ?>
+        <div class="vssl-stripe--videoembed--meta">
+            <?php if (!empty($caption['html'])) : ?>
+            <div class="vssl-stripe--videoembed--caption"><?= $this->inline($caption['html']) ?></div>
+            <?php endif; ?>
+            <?php if (!empty($credit['html'])) : ?>
+            <div class="vssl-stripe--videoembed--credit"><?= $this->inline($credit['html']) ?></div>
+            <?php endif; ?>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
 <?php endif; ?>
