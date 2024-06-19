6b25625bf2fee0c974b8a5bec82e6c8a14574be7
Joshua Bryant <joshb@journeygroup.com>
2024-06-19 14:46:28 -0400

    feat(table): better formatting for table caption

--- templates/stripes/stripe-table.php
+++ templates/stripes/stripe-table.php
@@ -5,22 +5,22 @@
 >
   <div class="vssl-stripe-column">
     <table>
-      <?php if (!empty($caption)) : ?>
-        <caption><?= $caption ?></caption>
+      <?php if (!empty($caption['html'])) : ?>
+        <caption><?= $this->inline($caption['html']) ?></caption>
       <?php endif; ?>
 
-      <?php if ($hasHeadersInFirstRow && !$hasHeadersInFirstColumn): ?>
+      <?php if ($hasHeadersInFirstRow && !$hasHeadersInFirstColumn) : ?>
         <thead>
-          <?php foreach ($dataset[0] as $item): ?>
+          <?php foreach ($dataset[0] as $item) : ?>
             <th><?= $item ?></th>
           <?php endforeach; ?>
         </thead>
       <?php endif; ?>
 
       <tbody>
-        <?php if ($hasHeadersInFirstRow && $hasHeadersInFirstColumn): ?>
+        <?php if ($hasHeadersInFirstRow && $hasHeadersInFirstColumn) : ?>
           <tr>
-            <?php foreach ($dataset[0] as $item): ?>
+            <?php foreach ($dataset[0] as $item) : ?>
               <th><?= $item ?></th>
             <?php endforeach; ?>
           </tr>
@@ -28,17 +28,19 @@
 
         <?php
           $rows = $hasHeadersInFirstRow ? array_slice($dataset, 1) : $dataset;
-          foreach ($rows as $row):
+          foreach ($rows as $row) :
         ?>
           <tr>
-            <?php foreach ($row as $index => $item): ?>
+            <?php foreach ($row as $index => $item) : ?>
               <?php $tag = ($index === 0 && $hasHeadersInFirstColumn) ? 'th' : 'td'; ?>
               <<?= $tag ?>>
                 <?= $item ?>
               </<?= $tag ?>>
             <?php endforeach; ?>
           </tr>
-        <?php endforeach; ?>
+        <?php
+          endforeach;
+        ?>
       </tbody>
     </table>
   </div>
