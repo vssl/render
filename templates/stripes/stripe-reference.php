001b012717ba2bf22aec6e991e4e9638668203ae
Brian McMillen <brian.mcmillen@journeygroup.com>
2024-06-20 15:11:32 -0400

    fix: update template name default string

--- templates/stripes/stripe-reference.php
+++ templates/stripes/stripe-reference.php
@@ -5,7 +5,7 @@
   if ($this->engine->exists("$referenceTypeTemplate")) {
     $this->insert($referenceTypeTemplate, get_defined_vars());
   } else {
-    $this->insert("$template_name--default", get_defined_vars());
+    $this->insert($template_name . '--default', get_defined_vars());
   }
   ?>
 </div>
