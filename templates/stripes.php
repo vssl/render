8504d72b809926fe4bc9fa105b05c50e8a709fce
Emily Merricks <emily.merricks@journeygroup.com>
2024-06-24 16:21:46 -0400

    fix(render): fix stripes.php template

--- templates/stripes.php
+++ templates/stripes.php
@@ -1,11 +1,7 @@
-<div class="vssl-page" data-id="<?= $this->e($id) ?>" data-type="<?= $this->e($type) ?>">
-    <?php if ($stripes) : ?>
-    <div class="vssl-stripes">
-        <?php foreach ($stripes as $index => $stripe) : ?>
-            <?php $stripe['stripe_index'] = $index; ?>
-            <?php $stripe['template_name'] = $themePrefix . 'stripes/' . $stripe['type'] ?>
-            <?= $this->insert($stripe['template_name'], $stripe) ?>
-        <?php endforeach; ?>
-    </div>
-    <?php endif; ?>
-</div>
+<?php if ($stripes) : ?>
+    <?php foreach ($stripes as $index => $stripe) : ?>
+        <?php $stripe['stripe_index'] = $index; ?>
+        <?php $stripe['template_name'] = $themePrefix . 'stripes/' . $stripe['type'] ?>
+        <?= $this->insert($stripe['template_name'], $stripe) ?>
+    <?php endforeach; ?>
+<?php endif; ?>
