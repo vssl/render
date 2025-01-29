1eb2e718d46360bccdd444e485e839f2b005bb33
Joshua Bryant <joshb@journeygroup.com>
2025-01-29 14:29:10 -0500

    fix(stripe-menu): do not re-declare template function

--- templates/stripes/stripe-menu.php
+++ templates/stripes/stripe-menu.php
@@ -1,19 +1,21 @@
 <?php
-function vsslStripeMenuLinkList($links, $depth = 1)
-{
-    $list = '<ul data-depth="' . $depth . '">';
-    foreach ($links as $link) {
-        if (!empty($link['link']) && (!empty($link['title']) || !empty($link['page_title']))) {
-            $list .= '<li class="vssl-stripe--menu--listitem" data-depth="' . $depth . '">'
-                . '<a href="' . $link['link'] . '" class="vssl-stripe--menu--link" data-depth="' . $depth . '">'
-                . '<span class="vssl-stripe--menu--link--text">' . ($link['title'] ?? $link['page_title']) . '</span>'
-                . '</a>'
-                . (!empty($link['links_nested']) ? vsslStripeMenuLinkList($link['links_nested'], $depth + 1) : '')
-                . '</li>';
+if (!function_exists('vsslStripeMenuLinkList')) {
+    function vsslStripeMenuLinkList($links, $depth = 1)
+    {
+        $list = '<ul data-depth="' . $depth . '">';
+        foreach ($links as $link) {
+            if (!empty($link['link']) && (!empty($link['title']) || !empty($link['page_title']))) {
+                $list .= '<li class="vssl-stripe--menu--listitem" data-depth="' . $depth . '">'
+                    . '<a href="' . $link['link'] . '" class="vssl-stripe--menu--link" data-depth="' . $depth . '">'
+                    . '<span class="vssl-stripe--menu--link--text">' . ($link['title'] ?? $link['page_title']) . '</span>'
+                    . '</a>'
+                    . (!empty($link['links_nested']) ? vsslStripeMenuLinkList($link['links_nested'], $depth + 1) : '')
+                    . '</li>';
+            }
         }
+        $list .= '</ul>';
+        return $list;
     }
-    $list .= '</ul>';
-    return $list;
 }
 
 if (count($menu_links)) : ?>
